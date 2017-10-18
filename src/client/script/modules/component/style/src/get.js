64e1f795773700945ecee7bce3cdd8abe0c3f299
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2017-10-18 21:26:26 +0700

    Fix bug

--- src/client/script/modules/component/style/src/get.js
+++ src/client/script/modules/component/style/src/get.js
@@ -1,13 +1,17 @@
 module.exports = function get(property, self, defaultValue = undefined) {
     let value;
-    if (property == 'size') {
+    if (property === 'size') {
         value = Number(self.$el.getAttribute('size'));
         if (!value) {
-            let closest = self.$el.closest('[size]');
-            if (closest) {
-                value = Number(closest.getAttribute('size'));
-            }
+            value = get('parentSize', self, defaultValue);
         }
+    } else if (property === 'parentSize') {
+        let closest = self.$el;
+        do {
+            closest = closest.parentElement;
+            if (closest == null) break;
+            value = closest.getAttribute('size');
+        } while (value == null);
     } else {
         console.warn('property is undefined, value will be set to defaultValue');
     }
